33878d792b37811009d0223d221fbebd3138961c
Ben Liblit <liblit@cs.wisc.edu>
2014-07-03 14:42:07 -0500

    Help Emacs flycheck find the local LLVM installation on CS machines.

--- .dir-locals.el
+++ .dir-locals.el
@@ -3,6 +3,7 @@
    "__STDC_CONSTANT_MACROS"
    "__STDC_FORMAT_MACROS"
    "__STDC_LIMIT_MACROS")
+  (flycheck-clang-include-path "/p/polyglot/public/llvm/install/include")
   (flycheck-clang-language-standard . "c++11")))
 
 ;; Local variables:
