44ebc6d74eb3172d19acc4156bf1ce8fced05102
Alisa Maas <ajmaas@cs.wisc.edu>
2014-07-29 17:47:42 -0500

    Added some basic tests with multiple array arguments, including one test with multiple functions to verify that the arguments are not being accumulated across functions (they aren't). Added and verified expected files for each test.

---
+++ tests/sentinelCheckTests/FindSentinelCheck18.c
@@ -0,0 +1,20 @@
+/**
+* This check tests we print info about both
+* array arguments when two array arguments are
+* given, and both have different sentinel checks
+*
+* We expect to find one non-optional sentinel check for string 
+* and 1 for string2.
+**/
+void printc(char c){}
+int find(char string[], char string2[], int distance)
+{
+  for(int i = 0; i < distance; i++){
+		if(string[i] == '\0'){
+			break;
+		}
+		if(string2[i] == '\0')
+			break;
+	}
+  return 1;
+}
---
+++ tests/sentinelCheckTests/FindSentinelCheck21.c
@@ -0,0 +1,31 @@
+/**
+* This check tests we correctly do not mix up array
+* arguments across functions.
+*
+* We expect to find one non-optional sentinel check for string 
+* and 1 for string2 in both foo and find.
+**/
+
+void printc(char c){}
+int foo(char string[], char string2[], int distance)
+{
+  for(int i = 0; i < distance; i++){
+		if(string[i] == '\0'){
+			break;
+		}
+		if(string2[i] == '\0')
+			break;
+	}
+  return 1;
+}
+int find(char string[], char string2[], int distance)
+{
+  for(int i = 0; i < distance; i++){
+		if(string[i] == '\0'){
+			break;
+		}
+		if(string2[i] == '\0')
+			break;
+	}
+  return 1;
+}
