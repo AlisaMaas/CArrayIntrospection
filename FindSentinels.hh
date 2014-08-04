6b9e17e1423507cea2f8ef70897f4f14dff05e50
Ben Liblit <liblit@cs.wisc.edu>
2014-08-04 11:32:24 -0500

    Don't indent access modifier labels, such as "public:".

--- FindSentinels.hh
+++ FindSentinels.hh
@@ -11,15 +11,15 @@ typedef std::unordered_set<const llvm::BasicBlock *> BlockSet;
 typedef std::unordered_map<const llvm::Argument *, std::pair<BlockSet, bool>> ArgumentToBlockSet;
 
 class FindSentinels : public llvm::ModulePass {
-	public:
-		FindSentinels();
-		static char ID;
-		void getAnalysisUsage(llvm::AnalysisUsage &) const final;
-		const std::unordered_map<llvm::BasicBlock const *, ArgumentToBlockSet>& getResultsForFunction(const llvm::Function*) const;
-		bool runOnModule(llvm::Module &) override final;
-		void print(llvm::raw_ostream &, const llvm::Module *) const;
-	private:
-		std::unordered_map<const llvm::Function *, std::unordered_map<const llvm::BasicBlock *, ArgumentToBlockSet>> allSentinelChecks;
-	};
+public:
+	FindSentinels();
+	static char ID;
+	void getAnalysisUsage(llvm::AnalysisUsage &) const final;
+	const std::unordered_map<llvm::BasicBlock const *, ArgumentToBlockSet>& getResultsForFunction(const llvm::Function*) const;
+	bool runOnModule(llvm::Module &) override final;
+	void print(llvm::raw_ostream &, const llvm::Module *) const;
+private:
+	std::unordered_map<const llvm::Function *, std::unordered_map<const llvm::BasicBlock *, ArgumentToBlockSet>> allSentinelChecks;
+};
 
 #endif // !INCLUDE_FIND_SENTINELS_HH
