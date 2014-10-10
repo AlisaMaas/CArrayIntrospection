e45c35c74537d9aad85ffde844e45c877a24804a
Ben Liblit <liblit@cs.wisc.edu>
2014-10-10 17:46:29 -0500

    Allow "-iiglue-read-file" to be used multiple times to read multiple files.
    This looks like a giant change, but really not.  Most of the changed lines
    are just added indentation due to wrapping a loop around existing logic.

--- IIGlueReader.cc
+++ IIGlueReader.cc
@@ -24,11 +24,11 @@ namespace {
 		     "Read iiglue analysis results and add them as metadata on corresponding LLVM entities",
 		     true, true);
 
-	static cl::opt<string>
-	iiglueFileName("iiglue-read-file",
-		       cl::Required,
-		       cl::value_desc("filename"),
-		       cl::desc("Filename containing iiglue analysis results"));
+	static cl::list<string>
+	iiglueFileNames("iiglue-read-file",
+			cl::OneOrMore,
+			cl::value_desc("filename"),
+			cl::desc("Filename containing iiglue analysis results; use multiple times to read multiple files"));
 }
 
 
@@ -39,54 +39,58 @@ void IIGlueReader::getAnalysisUsage(AnalysisUsage &usage) const {
 
 
 bool IIGlueReader::runOnModule(Module &module) {
-	// read entire JSON-formatted iiglue output as property tree
-	ptree root;
-	json_parser::read_json(iiglueFileName, root);
+	// iterate over iiglue files we've been asked to read
+	for (const string &iiglueFileName : iiglueFileNames) {
 
-	// iterate over iiglue-recognized library functions
-	const ptree &libraryFunctions = root.get_child("libraryFunctions");
-	for (const auto &functionInfo : libraryFunctions | map_values) {
-		// find corresponding LLVM function object
-		const string name = functionInfo.get<string>("foreignFunctionName");
-		const Function * const function = module.getFunction(name);
-		if (!function) {
-			errs() << "warning: found function " << name << " in iiglue results but not in bitcode\n";
-			continue;
-		}
+		// read entire JSON-formatted iiglue output as property tree
+		ptree root;
+		json_parser::read_json(iiglueFileName, root);
 
-		// focus on function parameters, and check for arity mismatch
-		const ptree &foreignFunctionParameters = functionInfo.get_child("foreignFunctionParameters");
-		const Function::ArgumentListType &args = function->getArgumentList();
-		if (foreignFunctionParameters.size() != args.size()) {
-			errs() << "warning: function " << name << " has " << foreignFunctionParameters.size() << " arguments in iiglue results but " << args.size() << " arguments in bitcode\n";
-			continue;
-		}
+		// iterate over iiglue-recognized library functions
+		const ptree &libraryFunctions = root.get_child("libraryFunctions");
+		for (const auto &functionInfo : libraryFunctions | map_values) {
+			// find corresponding LLVM function object
+			const string name = functionInfo.get<string>("foreignFunctionName");
+			const Function * const function = module.getFunction(name);
+			if (!function) {
+				errs() << "warning: found function " << name << " in iiglue results but not in bitcode\n";
+				continue;
+			}
 
-		// iterate across each parameter and corresponding annotations
-		const auto parameterAnnotations =
-			foreignFunctionParameters
-			| map_values
-			| transformed([](const ptree &pt) {
-					return pt.get_child("parameterAnnotations");
-				})
-			;
-		for (const auto &slot : boost::combine(parameterAnnotations, args)) {
-			// extract just the names (tags) of each annotation
-			const auto annotationTags =
-				slot.get<0>()
+			// focus on function parameters, and check for arity mismatch
+			const ptree &foreignFunctionParameters = functionInfo.get_child("foreignFunctionParameters");
+			const Function::ArgumentListType &args = function->getArgumentList();
+			if (foreignFunctionParameters.size() != args.size()) {
+				errs() << "warning: function " << name << " has " << foreignFunctionParameters.size() << " arguments in iiglue results but " << args.size() << " arguments in bitcode\n";
+				continue;
+			}
+
+			// iterate across each parameter and corresponding annotations
+			const auto parameterAnnotations =
+				foreignFunctionParameters
 				| map_values
 				| transformed([](const ptree &pt) {
-						assert(pt.size() == 1);
-						return pt.front();
+						return pt.get_child("parameterAnnotations");
 					})
-				| map_keys
 				;
+			for (const auto &slot : boost::combine(parameterAnnotations, args)) {
+				// extract just the names (tags) of each annotation
+				const auto annotationTags =
+					slot.get<0>()
+					| map_values
+					| transformed([](const ptree &pt) {
+							assert(pt.size() == 1);
+							return pt.front();
+						})
+					| map_keys
+					;
 
-			// PAArray annotation means iiglue thinks this is an array;
-			// ignore inferred array dimensionality: not needed yet
-			if (find(annotationTags, "PAArray") != end(annotationTags)) {
-				arrays.insert(&slot.get<1>());
-				atLeastOneArrayArg.insert(function);
+				// PAArray annotation means iiglue thinks this is an array;
+				// ignore inferred array dimensionality: not needed yet
+				if (find(annotationTags, "PAArray") != end(annotationTags)) {
+					arrays.insert(&slot.get<1>());
+					atLeastOneArrayArg.insert(function);
+				}
 			}
 		}
 	}
