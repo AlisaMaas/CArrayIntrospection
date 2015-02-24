71443fcf84756e739d4d4f3783af87cca9c48830
Alisa Maas <ajmaas@cs.wisc.edu>
2015-02-24 17:59:27 -0600

    Updated FindLengthChecks to provide access to results.

--- FindLengthChecks.hh
+++ FindLengthChecks.hh
@@ -19,12 +19,25 @@ public:
 	void getAnalysisUsage(llvm::AnalysisUsage &) const final;
 	bool runOnModule(llvm::Module &) override final;
 	void print(llvm::raw_ostream &, const llvm::Module *) const;
+	// access to analysis results derived by this pass
+	typedef std::pair<ArgumentToMaxIndexMap*, LengthArgumentMap*> FunctionLengthResults;
+	const FunctionLengthResults getResultsForFunction(const llvm::Function *) const;
 private:
 	std::map<llvm::Function const*, ArgumentToMaxIndexMap> maxIndexes;
 	std::map<llvm::Function const*, LengthArgumentMap> lengthArguments;
 };
 
+////////////////////////////////////////////////////////////////////////
 
+inline const FindLengthChecks::FunctionLengthResults FindLengthChecks::getResultsForFunction(const llvm::Function *func) const {
+	ArgumentToMaxIndexMap* first;
+	LengthArgumentMap* second;
+	const auto findConst = maxIndexes.find(func);
+	findConst == maxIndexes.end() ? nullptr : &findConst->second;
 
+	const auto findParam = lengthArguments.find(func);
+	findParam == lengthArguments.end() ? nullptr : &findConst->second;
+	return std::pair<ArgumentToMaxIndexMap*, LengthArgumentMap*>(first, second);
+}
 
 #endif // !INCLUDE_FIND_LENGTH_CHECKS_HH
