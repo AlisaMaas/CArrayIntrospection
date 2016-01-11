d6a798e1afef72c0c518091216e89f8b0e369fdd
Ben Liblit <liblit@cs.wisc.edu>
2016-01-10 19:04:11 -0600

    Add ValueSetSet shared_ptr specialization
    We're not actually using this yet anywhere, but I think we may soon.
    The plan is that eventually the other two specializations will no longer
    be used anywhere, at which point ValueSetSet can be demoted from a
    template to a regular class that always uses shared_ptr for its
    elements.

--- ValueSetSet.cc
+++ ValueSetSet.cc
@@ -19,3 +19,12 @@ const ValueSet* ValueSetSet<ValueSet>::getValueSetFromValue(const llvm::Value *v
 
         return nullptr;
 }
+
+
+std::shared_ptr<ValueSet> ValueSetSet<std::shared_ptr<ValueSet>>::getValueSetFromValue(const llvm::Value *value) const {
+        for (const auto &valueSet : *this)
+		if (valueSet && valueSet->count(value))
+			return valueSet;
+
+        return { };
+}
--- ValueSetSet.hh
+++ ValueSetSet.hh
@@ -1,6 +1,7 @@
 #ifndef INCLUDE_VALUE_SET_SET_HH
 #define INCLUDE_VALUE_SET_SET_HH
 
+#include <memory>
 #include "ValueSet.hh"
 
 namespace llvm  {
@@ -11,6 +12,12 @@ namespace llvm  {
 template <class VS = const ValueSet *>
 struct ValueSetSet : public std::set<VS> {
 	const ValueSet *getValueSetFromValue(const llvm::Value *) const;
+};
+
+
+template <>
+struct ValueSetSet<std::shared_ptr<ValueSet>> : public std::set<std::shared_ptr<ValueSet>> {
+	std::shared_ptr<ValueSet> getValueSetFromValue(const llvm::Value *) const;
 };
 
 
